95173b1e2920819b15d2e32797c048ff4428ac2c
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-08-06 22:50:58 +0530

    Updated

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,60 +1,53 @@
 import java.util.*;
-class SelectionSort{
+class BubbleSort
+{
     int compare = 0;
     int swap = 0;
-
     void sort(int arr[])
     {
         int n = arr.length;
-
         for (int i = 0; i < n-1; i++)
         {
-            int min_idx = i;
-            for (int j = i+1; j < n; j++){
+            for (int j = 0; j < n-i-1; j++)
+            {
                 compare++;
-                if (arr[j] > arr[min_idx])
-                min_idx = j;
+                if (arr[j] > arr[j+1])
+                {
+                    swap++;
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+
             }
-            if(min_idx != i){
-                swap++;
-                int temp = arr[min_idx];
-                arr[min_idx] = arr[i];
-                arr[i] = temp;
-            }
+
         }
     }
 
     void revsort(int arr[])
     {
         int n = arr.length;
-
         for (int i = 0; i < n-1; i++)
-        {
-            int min_idx = i;
-            for (int j = i+1; j < n; j++){
-                compare++;
-                if (arr[j] < arr[min_idx])
-                min_idx = j;
-            }
-            if(min_idx != i){
-                swap++;
-                int temp = arr[min_idx];
-                arr[min_idx] = arr[i];
-                arr[i] = temp;
-            }
-        }
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] < arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
     }
 
     void printArray(int arr[])
     {
         int n = arr.length;
         for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
+            System.out.print(arr[i] + " ");
         System.out.println();
     }
 
+
     public static void main(String args[]){
-        SelectionSort ob = new SelectionSort();
+        BubbleSort ob = new  BubbleSort();
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number of Elements : ");
         int n = sc.nextInt();
@@ -65,21 +58,26 @@ class SelectionSort{
 
             arr[i] = r.nextInt(100);
         }
-        ob.sort(arr);
+        ob.revsort(arr);
+        System.out.println("\nTaking Reverse Sorted array for Worst Case.\n");
         ob.printArray(arr);
-        SelectionSort ob1 = new SelectionSort();
+        BubbleSort ob1 = new BubbleSort();
         double start = System.nanoTime();
         ob1.sort(arr);
         double end = System.nanoTime();
-        System.out.println("\n Time taken for best case = " + (end-start)/100000);
+        System.out.print("\nSorted Array ===>");
+        ob1.printArray(arr);
+        System.out.println("\n Time taken for worst case = " + (end-start)/100000);
         System.out.println("\n Swaps = " + ob1.swap);
         System.out.println("\n Comparisons = " + ob1.compare + "\n");
-        ob.revsort(arr);
-        SelectionSort ob2 = new SelectionSort();
+        BubbleSort ob2 = new BubbleSort();
+        System.out.println("\nTaking previously Sorted array for best Case.\n");
         start = System.nanoTime();
         ob2.sort(arr);
         end = System.nanoTime();
-        System.out.println("\n Time taken for worst case = " + (end-start)/100000);
+        System.out.print("\nSorted Array ===>");
+        ob.printArray(arr);
+        System.out.println("\n Time taken for best case = " + (end-start)/100000);
         System.out.println("\n Swaps = " + ob2.swap);
         System.out.println("\n Comparisons = " + ob2.compare + "\n");
     }
--- InsertionSort.java
+++ InsertionSort.java
@@ -8,6 +8,7 @@ class InsertionSort {
         for (int i = 1; i < n; ++i) {
             int key = arr[i];
             int j = i - 1;
+            compare++;
             while (j >= 0 && arr[j] > key) {
                 compare++;
                 swap++;
@@ -54,21 +55,26 @@ class InsertionSort {
 
             arr[i] = r.nextInt(100);
         }
-        ob.sort(arr);
+        ob.revsort(arr);
+        System.out.println("\nTaking Reverse Sorted array for Worst Case.\n");
         ob.printArray(arr);
         InsertionSort ob1 = new InsertionSort();
         double start = System.nanoTime();
         ob1.sort(arr);
         double end = System.nanoTime();
-        System.out.println("\n Time taken for best case = " + (end-start)/100000);
+        System.out.print("\nSorted Array ===>");
+        ob1.printArray(arr);
+        System.out.println("\n Time taken for worst case = " + (end-start)/100000);
         System.out.println("\n Swaps = " + ob1.swap);
         System.out.println("\n Comparisons = " + ob1.compare + "\n");
-        ob.revsort(arr);
         InsertionSort ob2 = new InsertionSort();
+        System.out.println("\nTaking previously Sorted array for best Case.\n");
         start = System.nanoTime();
         ob2.sort(arr);
         end = System.nanoTime();
-        System.out.println("\n Time taken for worst case = " + (end-start)/100000);
+        System.out.print("\nSorted Array ===>");
+        ob.printArray(arr);
+        System.out.println("\n Time taken for best case = " + (end-start)/100000);
         System.out.println("\n Swaps = " + ob2.swap);
         System.out.println("\n Comparisons = " + ob2.compare + "\n");
     }

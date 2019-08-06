4955ba797c543aa6c25e80d6fa041c0c0cbcf1ec
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-08-06 23:42:25 +0530

    Updated

--- BubbleSort.java
+++ BubbleSort.java
@@ -8,6 +8,7 @@ class BubbleSort
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
         {
+            boolean swapped = false;
             for (int j = 0; j < n-i-1; j++)
             {
                 compare++;
@@ -17,8 +18,12 @@ class BubbleSort
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                 }
 
+            }
+            if(swapped == false){
+                break;
             }
 
         }

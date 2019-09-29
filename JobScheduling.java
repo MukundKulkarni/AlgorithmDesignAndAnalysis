6541f5e2e419f348596b4cc2cc192e2be8e8259b
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-09-29 18:11:34 +0530

    Updated

---
+++ JobScheduling.java
@@ -0,0 +1,29 @@
+//Assume jobs are sorted in descending order of profit
+public class Job{
+    String id;
+    int deadline;
+    int profit;
+
+    Job(String tid, int tdeadline, int tprofit){
+        id = tid;
+        deadline = tdeadline;
+        profit = tprofit;
+    }
+        selectedJobs(Job[] tjobs, int n){
+
+        }
+
+
+}
+
+public class JobScheduling{
+    public static void main(String[] args) {
+        Job[] given_jobs = new Job[5];
+        given_jobs[0] = new Job("j1",3,6);
+        given_jobs[1] = new Job("j2",2,4);
+        given_jobs[2] = new Job("j3",1,3);
+        given_jobs[3] = new Job("j4",3,3);
+        given_jobs[4] = new Job("j5",2,2);
+
+    }
+}

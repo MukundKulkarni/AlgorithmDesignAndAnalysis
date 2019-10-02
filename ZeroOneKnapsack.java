b564cd101686662c7b4a78fe6f06213fb1b5c66f
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-10-02 20:54:31 +0530

    Updated

--- ZeroOneKnapsack.java
+++ ZeroOneKnapsack.java
@@ -5,20 +5,15 @@ class ZeroOneKnapsack{
         int[][] solution_matrix = new int[matrix_row][matrix_column];
 
         for(int i = 0; i < matrix_row ; i++){
-            solution_matrix[i][0] = 0;
-        }
-
-        for(int i = 0; i < matrix_column ; i++){
-            solution_matrix[0][i] = 0;
-        }
-
-        for(int i = 1; i < matrix_row ; i++){
-            for(int j = 1; j < matrix_column; j++){
-                if(weights[i - 1] == j){
-                    solution_matrix[i][j] = values[i - 1];
+            for(int j = 0; j < matrix_column; j++){
+                if(i == 0 || j == 0){
+                    solution_matrix[i][j] = 0;
+                }
+                else if(weights[i - 1] <= j){
+                    solution_matrix[i][j] = Integer.max(solution_matrix[i-1][j], values[i - 1] + solution_matrix[i - 1][j - weights[i-1]]);
                 }
                 else{
-                    solution_matrix[i][j] = Integer.max(solution_matrix[i-1][j], solution_matrix[i-1][j] + solution_matrix[i - 1][j - 1]);
+                    solution_matrix[i][j] = solution_matrix[i - 1][j];
                 }
             }
         }
@@ -45,9 +40,9 @@ class ZeroOneKnapsack{
             System.out.println();
         }
 
-        System.out.println("\nSELECTED COINS\n");
-/*
-        int i = available_denominations.length;
+        System.out.println("\nSELECTED WEIGHTS\n");
+
+        int i = weights.length;
         int j = total_value;
         while(i != 0 || j != 0){
             if(result[i][j] == result[i-1][j]){

cfc05e7b871e5d2e0bc82d5840ca9abfdf7a9024
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-10-05 13:46:36 +0530

    Updated

--- KruskalsMst.java
+++ KruskalsMst.java
@@ -1,19 +1,93 @@
-public class KruskalsMst{
+import java.util.*;
+import java.io.*;
+import java.lang.*;
 
-    makeMst(int graph[][]){
+public class KruskalsMst
+{
+    public static int cost[][];
+    public static int n;
+
+    public static void krush(int cost[][],int n){
+        int set[]=new int[10];
+        int mst[][]=new int[10][10];
+        int printcost[][]=new int[10][10];
+
+        int min;
+        int i,j;
+        int u = -1,v = -1;
+        int count = 0;
+        int sum = 0,k =0 ;
+
+        for(i = 0; i < n; i++){
+            set[i]=i;
+        }
+
+        while(count < n-1){
+            min=999;
+
+            for(i = 0;i < n ; i++){
+                for(j = 0;j < n; j++){
+                    if(cost[i][j] != 0 && cost[i][j] < min){
+                        min = cost[i][j];
+                        u = i;
+                        v = j;
+                    }
+                }
+            }
+
+            i = find(u,set);
+            j = find(v,set);
+
+            if(i != j){
+
+                mst[k][0] = u;
+                mst[k][1] = v;
+                k++;
+                count++;
+                sum += min;
+                set[v] = u;
+            }
+
+            printcost[u][v] = cost[u][v];
+
+            cost[u][v] = cost[v][u] = 999;
+        }
+
+        if(sum>=999){
+            System.out.println("\nSpanning tree doesnt exist");
+        }
+        else{
+            System.out.println("\nThe edges selected for MST are:");
+            for(i=0;i<n-1;i++){
+                System.out.printf("\nEdge%d :  %d----%d   cost = %d",(i+1),mst[i][0],mst[i][1],printcost[mst[i][0]][mst[i][1]]);
+            }
+            System.out.println("\nMin cost of Spanning tree : "+sum);
+        }
 
     }
 
-    public static void main(String[] args) {
-        int graph[][] = new int[][] { { 0, 2, 0, 6, 0 },
-                                      { 2, 0, 3, 8, 5 },
-                                      { 0, 3, 0, 0, 7 },
-                                      { 6, 8, 0, 0, 9 },
-                                      { 0, 5, 7, 9, 0 } };
+    public static int find(int x,int set[]){
+        while(set[x] != x){
+            x = set[x];
+        }
+                                                                         /* To check the nodes results in circuit or closed path */
+    return x;
+    }
+    public static void main(String args[]){
+        cost=new int[10][10];
+        int i, j, n;
 
-        KruskalsMst g = new KruskalsMst();
+        Scanner in=new Scanner(System.in);
 
-        g.makeMst(graph);
+        System.out.print("\nEnter the no. of vertices: ");
+        n=in.nextInt();
 
+        System.out.print("\nEnter the adjacency matrix:\n");
+        for(i = 0; i < n; i++){
+            for(j = 0; j < n; j++){
+                cost[i][j]=in.nextInt();
+            }
+        }
+        krush(cost,n);
     }
 }
